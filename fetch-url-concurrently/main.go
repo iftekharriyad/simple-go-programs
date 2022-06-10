484299700114fef2f979e9d2f3535ed4017a79a6
Iftekhar Riyad <iftekharriyad@protonmail.com>
2022-06-10 16:47:29 +0600

    Add files via upload

---
+++ fetch-url-concurrently/main.go
@@ -0,0 +1,43 @@
+//Fetches URLs in parallel and reports their times and sizes.
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	ch := make(chan string)
+	for _, url := range os.Args[1:] {
+		go fetch(url, ch)
+	}
+	for range os.Args[1:] {
+		fmt.Println(<-ch)
+	}
+	fmt.Printf("Time elapsed: %f\n", time.Since(start).Seconds())
+}
+
+func fetch(url string, ch chan string) {
+	if !strings.HasPrefix(url, "https://") {
+		url = strings.Join([]string{"https://", url}, "")
+	}
+	start := time.Now()
+	resp, err := http.Get(url)
+	if err != nil {
+		ch <- fmt.Sprint(err)
+		return
+	}
+	nbytes, err := io.Copy(io.Discard, resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		ch <- fmt.Sprintf("while reading %s: %v", url, err)
+		return
+	}
+	secs := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%fs %dK %s", secs, nbytes/1024, url)
+}
