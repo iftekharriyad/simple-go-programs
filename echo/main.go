01c92438b1f667e2361c52abe5f451a499231582
Iftekhar Riyad <iftekharriyad@protonmail.com>
2022-06-24 14:47:53 +0600

    Add files via upload

---
+++ echo/main.go
@@ -0,0 +1,19 @@
+// It prints its command-line arguments.
+package main
+
+import (
+	"fmt"
+	"flag"
+	"strings"
+)
+
+var n = flag.Bool("n", false, "omit trailing newline")
+var sep = flag.String("s", " ", "separator")
+
+func main(){
+	flag.Parse()
+	fmt.Print(strings.Join(flag.Args(), *sep))
+	if !*n {
+		fmt.Println()
+	}
+}
